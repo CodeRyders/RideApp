f24afc2ba1e366b1791da453d890e1e773defedc
Fabian-IF <b3-github@fabianweiss.dev>
2024-12-17 10:47:26 +0100

    Fixed Cors

--- src/main/java/de/coderyders/rideapp/RideAppApplication.java
+++ src/main/java/de/coderyders/rideapp/RideAppApplication.java
@@ -3,10 +3,12 @@ package de.coderyders.rideapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:4200")
 public class RideAppApplication {
 
     public static void main(String[] args) {
